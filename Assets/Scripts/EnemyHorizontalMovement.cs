3c1ccabf596ec109decb0fce9e3e55c56ce26efb
LEARNET\joel.carlsson1 <joel.carlsson@elev.ga.lbs.se>
2018-11-20 10:50:25 +0100

    Enemy update
    Added enemies

---
+++ Assets/Scripts/EnemyHorizontalMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHorizontalMovement : MonoBehaviour
+{
+    public float speed = 2f;
+    private Rigidbody2D rbody;
+    public bool left = true;
+    public GroundChecker check;
+    // Use this for initialization
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        left = !left;
+
+    }
+    private void FixedUpdate()
+    {
+        
+        if (left == true)
+        {
+            rbody.MovePosition(rbody.position + (-(Vector2)transform.right * Time.deltaTime * speed));
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            rbody.MovePosition(rbody.position + ((Vector2)transform.right * Time.deltaTime * speed));
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (check.isGrounded == false)
+        {
+            left = !left;
+        }
+    }
+}
